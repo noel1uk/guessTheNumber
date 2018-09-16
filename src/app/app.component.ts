b29d6aef0e632c0087d4e7d6372c5de1ad3cf7cf
Noel Vock <noel1uk@gmail.com>
2018-09-16 01:35:22 +0100

    feat: add constructor and initial method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,4 +34,16 @@ export class AppComponent {
   noOfTries: number;
   original: number;
   guess: number;
+  
+  constructor() {
+    this.initializeGame();
+  }
+
+  initializeGame() {
+    this.noOfTries = 0;
+    this.original = Math.floor((Math.random() * 1000) + 1);
+    this.guess = null;
+    this.deviation = null;
+  }
 }
+
